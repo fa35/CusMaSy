7bb5a72cf2d0a918cd29dcb8f09c6368185d6296
luxfinis@LUXFINIS <luxfinis@ruggedinbox.com>
2015-09-10 20:07:41 +0200

    added code to switch between tui and gui

--- Source/CusMaSy.Project/Program.cs
+++ Source/CusMaSy.Project/Program.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using CusMaSy.Project.Views;
 
 namespace CusMaSy.Project
 {
     static class Program
     {
+        static void Main()
+        {
+            Console.Write("Running the GUI...\nPress Enter To Abort");
+
+            if(true)
+                new System.Threading.Thread(() => { System.Windows.Forms.Application.Run(new Form2()); }).Start();
+
+            Console.ReadLine();
+            System.Windows.Forms.Application.Exit();
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+        //[STAThread]
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Form1());
+        //}
     }
 }
