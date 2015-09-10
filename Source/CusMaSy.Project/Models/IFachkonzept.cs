9777d98f476bd14a863d35773b84d966995ec95d
luxfinis@LUXFINIS <luxfinis@ruggedinbox.com>
2015-09-10 19:16:31 +0200

    initial

---
+++ Source/CusMaSy.Project/Models/IFachkonzept.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CusMaSy.Project.Models
+{
+    public interface IFachkonzept
+    {
+        void ErstelleShop();
+
+        void ErstelleArtikel();
+
+
+        void LadeShop(long shopId);
+
+        void LadeShops();
+    }
+}
