0755121ae2ea7c8c8f9ec7efedfa961e4866ef1d
steven beier <fcb_bo89@yahoo,de>
2015-09-10 22:29:20 +0200

    MySql connection test

--- Source/CusMaSy.Project/Program.cs
+++ Source/CusMaSy.Project/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CusMaSy.Project
 {
@@ -17,6 +18,44 @@ namespace CusMaSy.Project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            string myConnectionString = "SERVER=localhost;" +
+                            "DATABASE=as_projekt;" +
+                            "UID=root;" +
+                            "PASSWORD=;";
+            SelectAll(myConnectionString);
+            InsertRow(myConnectionString);
+            SelectAll(myConnectionString);
+            
+        }
+
+        public static void SelectAll(string myConnectionString)
+        {
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM test";
+            MySqlDataReader Reader;
+            connection.Open();
+            Reader = command.ExecuteReader();
+            while (Reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                    row += Reader.GetValue(i).ToString() + ", ";
+                Console.WriteLine(row);
+            }
+            connection.Close();
+        }
+
+        public static void InsertRow(string myConnectionString) 
+        {
+            MySqlConnection myConnection = new MySqlConnection(myConnectionString);
+            string myInsertQuery = "INSERT INTO test (name, id) Values('cool',3)";
+            MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+            myCommand.Connection = myConnection;
+            myConnection.Open();
+            myCommand.ExecuteNonQuery();
+            myCommand.Connection.Close();
         }
     }
 }
